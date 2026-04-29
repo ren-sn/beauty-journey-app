c632928b09f7e0a0a98df79ac81a318ebf6e4780
ren-sn <78792460+ren-sn@users.noreply.github.com>
2026-04-29 12:27:42 +0800

    添加/更新文件: frontend/src/SimpleTest.jsx

---
+++ frontend/src/SimpleTest.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SimpleTest() {
+  return (
+    <div className="min-h-screen bg-pink-50 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl text-pink-600 font-bold">测试页面 🌸</h1>
+        <p className="text-pink-500 mt-4">如果你能看到这个，说明前端在工作！</p>
+      </div>
+    </div>
+  );
+}
+
+export default SimpleTest;
